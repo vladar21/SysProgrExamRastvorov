4fed3b6a50b224dd9c16893ca917c5d1e90378e5
Vladar21 <vladar21@gmail.com>
2019-11-13 16:29:04 +0200

    First commit

--- SysProgrExamRastvorov/Form1.Designer.cs
+++ SysProgrExamRastvorov/Form1.Designer.cs
@@ -28,13 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.MaxFoldersBox = new System.Windows.Forms.TextBox();
+            this.MaxFilesBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(13, 59);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(492, 301);
+            this.treeView1.TabIndex = 0;
+            // 
+            // MaxFoldersBox
+            // 
+            this.MaxFoldersBox.Location = new System.Drawing.Point(628, 59);
+            this.MaxFoldersBox.Name = "MaxFoldersBox";
+            this.MaxFoldersBox.Size = new System.Drawing.Size(100, 20);
+            this.MaxFoldersBox.TabIndex = 1;
+            this.MaxFoldersBox.Text = "100";
+            // 
+            // MaxFilesBox
+            // 
+            this.MaxFilesBox.Location = new System.Drawing.Point(628, 111);
+            this.MaxFilesBox.Name = "MaxFilesBox";
+            this.MaxFilesBox.Size = new System.Drawing.Size(100, 20);
+            this.MaxFilesBox.TabIndex = 2;
+            this.MaxFilesBox.Text = "100";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(628, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "MaxFoldersBox";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(631, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "MaxFilesBox";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(411, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MaxFilesBox);
+            this.Controls.Add(this.MaxFoldersBox);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox MaxFoldersBox;
+        private System.Windows.Forms.TextBox MaxFilesBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- SysProgrExamRastvorov/Form1.cs
+++ SysProgrExamRastvorov/Form1.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +17,86 @@ namespace SysProgrExamRastvorov
         public Form1()
         {
             InitializeComponent();
+        }
+
+        int maxFiles, currentFiles = 0, maxFolders, currenttFolders = 0;
+
+        string root = "";
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                maxFolders = int.Parse(MaxFoldersBox.Text);
+                maxFiles = int.Parse(MaxFilesBox.Text);
+                currentFiles = currenttFolders = 0;
+                treeView1.Nodes.Clear();
+                FolderBrowserDialog fbd = new FolderBrowserDialog();
+                fbd.Description = "Выбор каталога для поиска";
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    // корневой каталог рекурсии
+                    root = fbd.SelectedPath;
+                    TreeNode TreeRoot = treeView1.Nodes.Add(root);
+                    recurseFind(root, TreeRoot);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\nили неправильно заполненные данные");
+            }
+        }
+
+        Regex reg = new Regex("^<dir>(.*)$");
+
+        void recurseFind(string path, TreeNode node)
+        {
+            string[] files = { };
+            string[] folders = { };
+            try
+            {
+                folders = Directory.GetDirectories(path);
+            }
+            catch
+            {
+            }
+            try
+            {
+                files = Directory.GetFiles(path);
+            }
+            catch
+            {
+            }
+            // список каталогов-узлов дерева
+            List<TreeNode> sub = new List<TreeNode>();
+            foreach (string i in folders)
+            {
+                sub.Add(node.Nodes.Add("<dir>" + i));
+            }
+            foreach (string i in files)
+                node.Nodes.Add(i);
+            currenttFolders += folders.Length;
+            currentFiles += files.Length;
+            if (currentFiles >= maxFiles || currenttFolders >= maxFolders)
+            {
+                return;
+            }
+            foreach (TreeNode i in sub)
+            {
+                Match match = reg.Match(i.Text);
+                if (match.Success)
+                {
+                    // выбираем группу связанную со скобками
+                    string nodeName = match.Groups[1].Value;
+                    // рекурсивно входим в указанную директорию
+                    recurseFind(nodeName, i);
+                }
+            }
         }
     }
 }
